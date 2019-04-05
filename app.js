c7ba7c6cf39751174f3bc92f41c4959c4a003844
Eddy Caldas <eddycaldas@live.com>
2019-04-05 08:10:41 -0600

    updated bodyParser

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const PORT = process.env.PORT ||8080
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = {
     employee: require('./routes/employee'),
